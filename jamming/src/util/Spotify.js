43146b77f85059e0670407ec61d39c93db3aca7c
Francisco Mateus <gtaskfm@gmail.com>
2019-03-13 19:16:35 +0100

    Created Spotify Access Token

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
-const Spotify = {
+const clientId = 5ba88baf49bb4dbeaaf790b1e0af91e8;
+let accessToken = '';
+const redirectUri = 'http://localhost:3000/';
+let wipeTimer;
 
+const Spotify = {
+  getAccessToken () {
+    if (accessToken) {
+      return accessToken;
+    } else { // verify token && get token time
+      let url = window.location.href;
+      let accessTokenMatch = url.match(/access_token=([^&]*)/);
+      let matchedExpireTime = url.match(/expires_in=([^&]*)/);
+
+      if (accessTokenMatch && matchedExpireTime) {
+
+        accessToken = accessTokenMatch[1]
+        let expireTime = matchedExpireTime[1]
+
+        wipeTimer = window.setTimeout(() => accessToken = '', expireTime * 1000);
+        window.history.pushState('Access Token', null, '/');
+        return accessToken;
+      } else { // redirect user to url
+        window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      }
+    }
 }
 
 export default Spotify;
